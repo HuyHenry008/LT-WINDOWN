fc4a4bd21a599d5286101acc9ef6ce24091dd15a
HuyHenry008 <giahuyphamthanh8@gmail.com>
2024-12-03 21:54:39 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace btvn
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Điểm bắt đầu chính của ứng dụng.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // Đảm bảo Form1 đã được định nghĩa
+        }
+    }
+}
